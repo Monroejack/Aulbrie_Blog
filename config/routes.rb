a461e31b85f538d845a732742b0a924f64ad87ee
Monroejack <Monroejack2121@gmail.com>
2018-03-15 08:46:40 -0400

    My Blog Project for my wife Aulbrie...this is my first draft

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  resources :comments
+  resources :posts
+  #get '/posts' => 'posts#index'
+  #get '/posts/:id' => 'posts#show'
+  #get '/posts/:id' => 'posts#edit'
+  #patch/put '/posts/:id' => 'posts#update'
+  #get '/posts/new' => 'posts#new'
+  #post '/posts' => 'posts/create'
+  #delete '/posts/:id' => 'posts#destroy'
+
+  get 'index' => 'posts#index'
+
+  get 'comments' => 'posts#comments'
+
+  root 'posts#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
